53224d44b3672ea43ce810fea631583055a6e496
Zheren Zhu <83292144+zherenz@users.noreply.github.com>
2025-01-02 23:24:28 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='air_download',
     version='0.1.0',
-    url='https://github.com/johncolby/air_download',
+    url='https://github.com/zherenz/air_download',
     author='John Colby',
     author_email='john.b.colby@gmail.com',
     description='Command line interface to the Automated Image Retrieval (AIR) Portal',
